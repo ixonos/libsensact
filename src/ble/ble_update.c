40a3b86a4735e0fb4fb28c552978a47422b59577
Petteri Tikander <petteri.tikander@ixonos.com>
2014-01-26 21:52:23 +0200

    Added detecting of ble-advertiments, and user-led control.
    User-led is flashing when waiting for advertisements from ble-device.
    Led will be switched when connected to ble-device.
    Led will be switched off when disconnecting from ble-device,

--- src/ble/ble_update.c
+++ src/ble/ble_update.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <syslog.h>
 #include <stdio.h>
 #include <string.h>
@@ -44,9 +45,125 @@ typedef struct ble_conn_t
     int rw_retry_cnt;
 } BLE_CONN_T;
 
-void ble_scan(void)
+void control_am335x_user_led0(unsigned char flag)
 {
-    int dd, err, dev_id;
+    FILE *fd;
+
+    // control user led 0
+    if (flag == 0) // led OFF
+    {
+        fd = fopen("/sys/class/leds/am335x\:EVM_SK\:usr0/brightness","rw+");
+        if (fd)
+        {
+            fwrite("0", 1, 1, fd); // OFF
+            fclose(fd);
+        }
+        //else syslog(LOG_ERR, "cannot open led-device");
+    }
+    if (flag == 1)  // led ON
+    {
+        fd = fopen("/sys/class/leds/am335x\:EVM_SK\:usr0/trigger","rw+");
+        if (fd)
+        {
+            fwrite("none", 1, 4, fd); // blinking off
+            fclose(fd);
+        }
+
+        fd = fopen("/sys/class/leds/am335x\:EVM_SK\:usr0/brightness","rw+");
+        if (fd)
+        {
+            fwrite("1", 1, 1, fd); // ON
+            fclose(fd);
+        }
+        //else syslog(LOG_ERR, "cannot open led-device");
+    }
+    if (flag == 2) // blink the led
+    {
+        //echo 100 >> /sys/class/leds/am335x\:EVM_SK\:usr0/delay_off
+        //echo 100 >> /sys/class/leds/am335x\:EVM_SK\:usr0/delay_on
+        // default: 1 sec ON/1 sec OFF
+        fd = fopen("/sys/class/leds/am335x\:EVM_SK\:usr0/trigger","rw+");
+        if (fd)
+        {
+            fwrite("timer", 1, 5, fd);
+            fclose(fd);
+        }
+        //else syslog(LOG_ERR, "cannot open led-device");	
+    }
+
+    return;
+}
+
+int monitor_advertisements(int hciSocket, unsigned int scan_time, char *ble_addr)
+{
+    fd_set rfds;
+    struct timeval tv;
+    int result = -1;
+    unsigned char hciEventBuf[HCI_MAX_EVENT_SIZE];
+    int len;
+    evt_le_meta_event *leMetaEvent;
+    le_advertising_info *leAdvertisingInfo;
+    char adv_ble_addr[18]; // advertised ble-address
+    struct hci_filter of, nf; // old/new filters
+    socklen_t olen;
+
+    olen = sizeof(of);
+
+    if (getsockopt(hciSocket, SOL_HCI, HCI_FILTER, &of, &olen) < 0) {
+        printf("Could not get socket options\n");
+        return -1;
+    }
+
+    hci_filter_clear(&nf);
+    hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
+    hci_filter_set_event(EVT_LE_META_EVENT, &nf);
+
+    if (setsockopt(hciSocket, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0) {
+        printf("Could not set socket options\n");
+        return -1;
+    }
+
+    FD_ZERO(&rfds);
+    FD_SET(hciSocket, &rfds);
+    tv.tv_sec = scan_time; // wait for poll_time seconds for advertisement event
+    tv.tv_usec = 0;
+
+    result = select(hciSocket + 1, &rfds, NULL, NULL, &tv);
+
+    if (result <= 0) {
+        if ( (result == -1) || (result == 0) ) {
+            printf("hci-socket read error: %s\n", (result == -1) ? strerror(errno) : "timeout");
+            syslog(LOG_ERR, "hci-socket read error: %s\n", (result == -1) ? strerror(errno) : "timeout");
+        }
+        else {
+            printf("hci-socket read error: %s\n", "undefined");
+            syslog(LOG_ERR, "hci-socket read error: %s\n", "undefined");
+        }
+        return -1;
+    }
+
+    // read received event
+    len = read(hciSocket, hciEventBuf, sizeof(hciEventBuf));
+    leMetaEvent = (evt_le_meta_event *)(hciEventBuf + (1 + HCI_EVENT_HDR_SIZE));
+    len -= (1 + HCI_EVENT_HDR_SIZE);
+
+    if ( (leMetaEvent->subevent != 0x02) || (len < 0) )
+        return -1;
+
+    leAdvertisingInfo = (le_advertising_info *)(leMetaEvent->data + 1);
+    ba2str(&leAdvertisingInfo->bdaddr, adv_ble_addr);
+    syslog(LOG_ERR, "advertisement event: ble-addr = %s", adv_ble_addr);
+    setsockopt(hciSocket, SOL_HCI, HCI_FILTER, &of, sizeof(of));
+
+    if (ble_addr != NULL)
+        strncpy(ble_addr, adv_ble_addr, sizeof(adv_ble_addr));
+
+    return 0;
+}
+
+int ble_scan(char *ble_adv_addr)
+{
+    int dd, err, adv_err, dev_id;
     uint8_t own_type = 0x00;
     uint8_t scan_type = 0x01;
     uint8_t filter_policy = 0x00;
@@ -60,7 +177,7 @@ void ble_scan(void)
     if (dd < 0)
     {
         perror("Could not open device");
-        return;
+        return -1;
     }
 
     err = hci_le_set_scan_parameters(dd, scan_type, interval, window,
@@ -80,15 +197,26 @@ void ble_scan(void)
     }
 
     printf("LE Scan ...\n");
-    sleep(BLE_SCAN_TIME);
+    adv_err = monitor_advertisements(dd, BLE_SCAN_TIME, ble_adv_addr);
+    //sleep(BLE_SCAN_TIME);
 
     // End scan
     err = hci_le_set_scan_enable(dd, 0x00, filter_dup, 1000);
-    if (err < 0)
-        perror("Disable scan failed");
+    if (err < 0 || adv_err < 0)
+    {
+        perror("Scanning or Disable scan failed");
+        goto error;
+    }
+
+    goto success;
+
+success:
+    hci_close_dev(dd);
+    return 0;
 
 error:
     hci_close_dev(dd);
+    return -1;
 }
 
 // This demo-program in simple way to adapt ble-interface to sensact-platform
@@ -109,8 +237,9 @@ void update_loop(char *ble_addr, float *temperature)
 
 
     struct ble_conn_t ble_conn;
-    int ret;
+    int ret = -1;
     double double_val;
+    char ble_adv_addr[18]; // advertisement address
 
     ble_conn.state = BLE_STATE_NOT_CONNECTED; // ble-connection should not be establishment yet
     strncpy(ble_conn.ble_addr, ble_addr, sizeof(ble_conn.ble_addr));
@@ -127,14 +256,28 @@ void update_loop(char *ble_addr, float *temperature)
                     printf("--------------------------------------------------------------------------\n");
 
                     // LE scan before connect (fixes "no route to host")
-                    ble_scan();
+                    memset(ble_adv_addr, 0, sizeof(ble_adv_addr));
+                    control_am335x_user_led0(2); // led blinking blink
+                    ret = ble_scan(ble_adv_addr);
 
-                    ret = ble_connect(ble_conn.ble_addr);
+                    if (ret >= 0)
+                    {
+                        if (!strcmp(ble_conn.ble_addr, ble_adv_addr)) // advertised address match with address given by parameter to update_loop
+                            ret = ble_connect(ble_conn.ble_addr);
+                        else 
+                            ret = -1;
+                    }
 
                     if (ret < 0) // connection establishment failed
+                    {
                         ble_conn.state = BLE_STATE_WF_RECONN;
+                        control_am335x_user_led0(0); // led off
+                    }
                     else
+                    {
                         ble_conn.state = BLE_STATE_CONNECTED;
+                        control_am335x_user_led0(1); // led on
+                    }
                 }
                 break;
 
@@ -164,7 +307,6 @@ void update_loop(char *ble_addr, float *temperature)
             case BLE_STATE_READ:
                 {
                     double_val = 0;
-
                     ret = ble_get_float("Temp", &double_val, TIMEOUT);
                     printf("ret: %d, sensor value: %lf\n", ret, double_val);
                     printf("--------------------------------------------------------------------------\n");
@@ -190,6 +332,7 @@ void update_loop(char *ble_addr, float *temperature)
                 {
                     printf("Disconnect\n");
                     ble_disconnect();
+                    control_am335x_user_led0(0); // led off
                     ble_conn.state = BLE_STATE_WF_RECONN;
                 }
                 break;
